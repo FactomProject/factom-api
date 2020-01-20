852c13ecd34107c46b92cc0e3037a821e2a3c4da
Sam Barnes <mistersamuelbarnes@gmail.com>
2020-01-20 09:52:54 -0600

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     author="Ben Homnick",
     author_email="bhomnick@gmail.com",
     name="factom-api",
-    version="1.0.3",
+    version="1.1.0",
     description="Python client library for the Factom API",
     long_description=long_description,
     license="MIT License",
